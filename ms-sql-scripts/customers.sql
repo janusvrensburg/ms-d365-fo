7a928aacad8f0c43da782ad9ef3ef78cec834442
Janus van Rensburg <janusvanrensburg@outlook.com>
2021-09-20 22:42:02 -0500

    Update customers.sql

--- ms-sql-scripts/customers.sql
+++ ms-sql-scripts/customers.sql
@@ -33,12 +33,42 @@ SELECT [CUS].[DataAreaID]                        AS [Company_ID]
        LEFT JOIN [dbo].[DirPartyTable] AS [CUS_NME]
               ON [CUS_NME].[RecID]     = [CUS].[Party]
              AND [CUS_NME].[Partition] = [CUS].[Partition]
+             
+       -- CUSTOMER GROUP LOOKUP
+       LEFT JOIN [dbo].[CustGroup] AS [CUS_GRP]
+              ON [CUS_GRP].[CustGroup]  = [CUS].[CustGroup]
+             AND [CUS_GRP].[Partition]  = [CUS].[Partition]
+             AND [CUS_GRP].[DataAreaID] = [CUS].[DataAreaID]
+             
+       -- CUSTOMER COMMISSION GROUP LOOKUP
+       LEFT JOIN [dbo].[CommissionCustomerGroup] AS [CUS_COM_GRP]
+              ON [CUS_COM_GRP].[GroupID]    = [CUS].[CommissionGroup]
+             AND [CUS_COM_GRP].[Partition]  = [CUS].[Partition]
+             AND [CUS_COM_GRP].[DataAreaID] = [CUS].[DataAreaID]
+             
+       -- CUSTOMER SALES DISTRICT LOOKUP
+       LEFT JOIN [dbo].[smmBusRelSalesDistrictGroup] AS [CUS_SLE_DIS]
+              ON [CUS_SLE_DIS].[SalesDistrictID] = [CUS].[SalesDistrictID]
+             AND [CUS_SLE_DIS].[Partition]       = [CUS].[Partition]
+             AND [CUS_SLE_DIS].[DataAreaID]      = [CUS].[DataAreaID]
 
        -- CUSTOMER SALES POOL LOOKUP
        LEFT JOIN [dbo].[SalesPool] AS [CUS_SLE_SPL]
               ON [CUS_SLE_SPL].[SalesPoolID] = [CUS].[SalesPoolID]
              AND [CUS_SLE_SPL].[Partition]   = [CUS].[Partition]
              AND [CUS_SLE_SPL].[DataAreaID]  = [CUS].[DataAreaID]
+             
+       -- CUSTOMER PAYMENT TERM LOOKUP
+       LEFT JOIN [dbo].[PaymTerm] AS [CUS_PAY_TRM]
+              ON [CUS_PAY_TRM].[PaymTermID] = [CUS].[PaymTermID]
+             AND [CUS_PAY_TRM].[Partition]  = [CUS].[Partition]
+             AND [CUS_PAY_TRM].[DataAreaID] = [CUS].[DataAreaID]
+
+       -- CUSTOMER DELIVERY MODE LOOKUP
+       LEFT JOIN [dbo].[DlvMode] AS [CUS_DEL_MDE]
+              ON [CUS_DEL_MDE].[Code]       = [CUS].[DlvMode]
+             AND [CUS_DEL_MDE].[Partition]  = [CUS].[Partition]
+             AND [CUS_DEL_MDE].[DataAreaID] = [CUS].[DataAreaID]
 
        -- CUSTOMER EMPLOYEE RESPONSIBLE LOOKUP
        LEFT JOIN [dbo].[HcmWorker] AS [CUS_EMP_RES]
@@ -48,37 +78,5 @@ SELECT [CUS].[DataAreaID]                        AS [Company_ID]
        -- CUSTOMER EMPLOYEE RESPONSIBLE (NAME) LOOKUP
        LEFT JOIN [dbo].[DirPartyTable] AS [CUS_EMP_RES_NME]
               ON [CUS_EMP_RES_NME].[RecID]     = [CUS_EMP_RES].[Person]
-             AND [CUS_EMP_RES_NME].[Partition] = [CUS].[Partition]
-
-       -- CUSTOMER DELIVERY MODE LOOKUP
-       LEFT JOIN [dbo].[DlvMode] AS [CUS_DEL_MDE]
-              ON [CUS_DEL_MDE].[Code]       = [CUS].[DlvMode]
-             AND [CUS_DEL_MDE].[Partition]  = [CUS].[Partition]
-             AND [CUS_DEL_MDE].[DataAreaID] = [CUS].[DataAreaID]
-
-       -- CUSTOMER PAYMENT TERM LOOKUP
-       LEFT JOIN [dbo].[PaymTerm] AS [CUS_PAY_TRM]
-              ON [CUS_PAY_TRM].[PaymTermID] = [CUS].[PaymTermID]
-             AND [CUS_PAY_TRM].[Partition]  = [CUS].[Partition]
-             AND [CUS_PAY_TRM].[DataAreaID] = [CUS].[DataAreaID]
-
-       -- CUSTOMER GROUP LOOKUP
-       LEFT JOIN [dbo].[CustGroup] AS [CUS_GRP]
-              ON [CUS_GRP].[CustGroup]  = [CUS].[CustGroup]
-             AND [CUS_GRP].[Partition]  = [CUS].[Partition]
-             AND [CUS_GRP].[DataAreaID] = [CUS].[DataAreaID]
-
-       -- CUSTOMER COMMISSION GROUP LOOKUP
-       LEFT JOIN [dbo].[CommissionCustomerGroup] AS [CUS_COM_GRP]
-              ON [CUS_COM_GRP].[GroupID]    = [CUS].[CommissionGroup]
-             AND [CUS_COM_GRP].[Partition]  = [CUS].[Partition]
-             AND [CUS_COM_GRP].[DataAreaID] = [CUS].[DataAreaID]
-
-       -- CUSTOMER SALES DISTRICT LOOKUP
-       LEFT JOIN [dbo].[smmBusRelSalesDistrictGroup] AS [CUS_SLE_DIS]
-              ON [CUS_SLE_DIS].[SalesDistrictID] = [CUS].[SalesDistrictID]
-             AND [CUS_SLE_DIS].[Partition]       = [CUS].[Partition]
-             AND [CUS_SLE_DIS].[DataAreaID]      = [CUS].[DataAreaID];
-
-GO
+             AND [CUS_EMP_RES_NME].[Partition] = [CUS].[Partition];
 
