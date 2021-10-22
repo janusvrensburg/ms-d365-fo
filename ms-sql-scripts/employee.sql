7941fc4c1df5f1ae810ea043620403a7945b78ae
Janus van Rensburg <janusvanrensburg@outlook.com>
2021-10-21 22:02:12 -0500

    Updates

--- ms-sql-scripts/employee.sql
+++ ms-sql-scripts/employee.sql
@@ -1,7 +1,10 @@
 
 SELECT [EMP].[RecID]                  AS [Employee_Record_ID]
+
       ,[EMP].[PersonnelNumber]        AS [Personnel_Number]
+
       ,NULLIF([EMP_NME].[Name], '')   AS [Full_Name]
+
   FROM [dbo].[HcmWorker] AS [EMP]
 
        LEFT JOIN [dbo].[DirPartyTable] AS [EMP_NME]
